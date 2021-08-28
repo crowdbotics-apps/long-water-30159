e7788656f38f3bf0c638247e1ebd96513eefcb4f
Crowdbotics <team@crowdbotics.com>
2021-08-28 07:38:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "long_water_30159Identifier",
+  url: "https://long_water_30159Identifier.botics.co",
+  api: "https://long_water_30159Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
